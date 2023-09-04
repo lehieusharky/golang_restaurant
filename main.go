aee64350a8295acef6de8c9f377a5cd2050673ef
Manhnedungso <myytb20095511@gmail.com>
2023-09-04 15:05:51 +0700

    [Authentication] Signup - Login

--- main.go
+++ main.go
@@ -1,25 +1,38 @@
 package main
 
 import (
+	"log"
 	"os"
-	"restaurant_management/database"
+
+	// "restaurant_management/database"
+	"restaurant_management/routes"
 
 	"github.com/gin-gonic/gin"
-	"go.mongodb.org/mongo-driver/mongo"
+	"github.com/joho/godotenv"
+	// "go.mongodb.org/mongo-driver/mongo"
 )
 
-var foodCollection *mongo.Collection = database.OpenCollection(database.Client, "food")
+// var foodCollection *mongo.Collection = database.OpenCollection(database.Client, "food")
 
 
 func main() {
+	err := godotenv.Load(".env")
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
 	port := os.Getenv("PORT")
 
-	if port == "" {
-		port = "8000"
+	if port==""{
+		port="8000"
 	}
 
 	router := gin.New()
 	router.Use(gin.Logger())
+
+	routes.AuthRoutes(router)
+	routes.UserRoutes(router)
+
 	// routes.UserRoutes(router)
 	// router.Use(middleware.Authentication())
 
